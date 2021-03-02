c56fa4c2930eaa8202084a3c39b217a24c57edcd
George4888 <dangeorge4888@gmail.com>
2021-03-02 12:11:33 +0200

    select All completed button and delete

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { DataContext } from "./DataProvider";
 
 const Info = () => {
+  const [checkAll, setCheckAll] = useState(false);
+  const [todos, setTodos] = useContext(DataContext);
+
+  const handleCheckAll = () => {
+    const newTodos = [...todos];
+    newTodos.forEach((todo) => {
+      todo.complete = !checkAll;
+    });
+    setTodos(newTodos);
+    setCheckAll(!checkAll);
+  };
+
+  const deleteTodo = () => {
+    const newTodos = todos.filter((todo) => {
+      return todo.complete === false;
+    });
+    setTodos(newTodos);
+    setCheckAll(false);
+  };
+
   return (
     <>
       <div className="row">
         <label htmlFor="all">
-          <input type="checkbox" name="all" id="all" />
+          <input
+            type="checkbox"
+            name="all"
+            id="all"
+            onChange={handleCheckAll}
+            checked={checkAll}
+          />
           All
         </label>
-        <p>You have 0 tasks to complete</p>
-        <button id="delete">Delete</button>
+        <p>You have {todos.length} tasks to complete</p>
+        <button id="delete" onClick={deleteTodo}>
+          Delete
+        </button>
       </div>
     </>
   );
